72cc038a2655dce508e02b4945bb09e75dae564e
DSquire9 <dss9593@rit.edu>
2025-02-20 16:29:58 -0500

    Tickets!

--- iwjha/requests.gd
+++ iwjha/requests.gd
@@ -10,23 +10,44 @@ const _POTION_TYPES = [
 	"Stealth Potion",
 	"All On Red Potion"
 	]
+	
+const _POTION_IMAGES = [
+	"Health Potion",
+	"Poison Potion",
+	"Alchohol Immunity Potion",
+	"Fire Resistance Potion",
+	"Romeo/Juliet Potion",
+	"Munster Energy Potion",
+	"Stealth Potion",
+	"All On Red Potion"
+	]
 
 var requests = ["","","",""]
+var tickets = [null,null,null,null]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var children = get_children()
+	var i = 0
+	for child in children:
+		if child is Ticket:
+			tickets[i] = child
+			i += 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_request_timer_timeout() -> void:
 	for n in range(4):
 		if requests[n] == "":
-			requests[n] = _POTION_TYPES.pick_random()
+			var type = randi() % requests.size()
+			requests[n] = _POTION_TYPES[type]
+			tickets[n].image.texture = load(_POTION_IMAGES[type])
+			tickets[n].label.text = _POTION_TYPES[type]
+			tickets[n].show()
 			print(requests)
 			return
 	# If the slots are full stop the timer
@@ -38,6 +59,7 @@ func _on_cauldron_potion_brewed(type: String) -> void:
 		for n in range(4):
 			if requests[n] == type:
 				requests[n] = ""
+				tickets[n].hide()
 				if $RequestTimer.is_stopped():
 					$RequestTimer.start()
 				break
---
+++ iwjha/ticket.gd
@@ -0,0 +1,13 @@
+class_name Ticket extends TextureRect
+
+var image = null
+var label = null
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	image = $Image
+	label = $Label
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
