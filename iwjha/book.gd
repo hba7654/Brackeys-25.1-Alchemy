7c35e12f6d9ed5dc8c910e8c2893875ed979518a
DSquire9 <dss9593@rit.edu>
2025-02-21 16:17:55 -0500

    added book and keys

---
+++ iwjha/book.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var _isOpen = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("open_book") and _isOpen:
+		_isOpen = false
+		$Big.hide()
+	elif Input.is_action_just_pressed("open_book") and !_isOpen:
+		_isOpen = true
+		$Big.show()
+		
+	if _isOpen:
+		if Input.is_action_just_pressed("page_left"):
+			pass
+		if Input.is_action_just_pressed("page_right"):
+			pass
