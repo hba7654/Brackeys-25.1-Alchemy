dc6fe6bbd04f56521213f4f88874d98fbe17b44c
DSquire9 <dss9593@rit.edu>
2025-02-21 23:32:26 -0500

    added pages

--- iwjha/book.gd
+++ iwjha/book.gd
@@ -1,6 +1,17 @@
 extends Node
 
 var _isOpen = false
+var _currPage = 1
+const PAGES = {
+	1: {"image": "", "Name": "Health Potion", "Ingredients": ""},
+	2: {"image": "", "Name": "Poison Potion", "Ingredients": ""},
+	3: {"image": "", "Name": "Alchohol Immunity Potion", "Ingredients": ""},
+	4: {"image": "", "Name": "Fire Resistance Potion", "Ingredients": ""},
+	5: {"image": "", "Name": "Romeo/Juliet Potion", "Ingredients": ""},
+	6: {"image": "", "Name": "Munster Energy Potion", "Ingredients": ""},
+	7: {"image": "", "Name": "Stealth Potion", "Ingredients": ""},
+	8: {"image": "", "Name": "All On Red Potion", "Ingredients": ""},
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,6 +29,8 @@ func _process(_delta: float) -> void:
 		
 	if _isOpen:
 		if Input.is_action_just_pressed("page_left"):
-			pass
+			if _currPage != 1:
+				_currPage -= 1
 		if Input.is_action_just_pressed("page_right"):
-			pass
+			if _currPage != 8:
+				_currPage += 1
